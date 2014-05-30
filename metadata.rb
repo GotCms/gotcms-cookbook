7639049f3e5979ef7cd5e944751841ec1622a38f
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2014-05-30 19:26:07 +0200

    Bump to 0.1.2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'pierre.rambaud86@gmail.com'
 license 'LGPLv3'
 description 'Installs/Configures GotCms'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.1'
+version '0.1.2'
 
 recipe 'gotcms', 'Installs and configures GotCms LAMP stack on a single system'
 
